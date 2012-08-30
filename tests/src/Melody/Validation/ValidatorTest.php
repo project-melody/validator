edc494eabceb9d289df3d3c29f4591f3f3dbf4a6
Marcelo Santos <marcelsud@gmail.com>
2012-08-30 20:43:38 -0300

    Update tests/src/Melody/Validation/ValidatorTest.php

--- tests/src/Melody/Validation/ValidatorTest.php
+++ tests/src/Melody/Validation/ValidatorTest.php
@@ -10,10 +10,9 @@ use Melody\Validation\Validator;
 
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
-	public function test_add_constraint()
-	{
-		$validator = new Validator();
-
-		$this->assertTrue($validator->addConstraint(new EmailConstraint()) instanceof Validator);
-	}
+    public function test_add_constraint()
+    {
+        $validator = new Validator();
+        $this->assertTrue($validator->addConstraint(new EmailConstraint()) instanceof Validator);
+    }
 }
