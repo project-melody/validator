4c1be0da5e82eecdee6fdf84628125de552d327b
Marcelo Santos <marcelsud@gmail.com>
2013-01-12 14:34:46 -0200

    Added Range(min,max) constraint

---
+++ src/Melody/Validation/Constraints/Range.php
@@ -0,0 +1,47 @@
+<?php
+namespace Melody\Validation\Constraints;
+
+class Range extends Constraint
+{
+    private $min;
+    private $max;
+
+    /**
+     * @param Int $min
+     * @param Int $max
+     * @throws \Exception
+     */
+    public function __construct($min, $max)
+    {
+        if (!is_int($min)) {
+            throw new \Exception("It is necessary to define the min as integer");
+        }
+
+        if (!is_int($max)) {
+            throw new \Exception("It is necessary to define the max as integer");
+        }
+
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    public function validate($input)
+    {
+        if (!is_int($input)) {
+            throw new \Exception("The input field must be a string");
+        }
+
+        return filter_var(
+                $input,
+                FILTER_VALIDATE_INT,
+                array(
+                        'options' => array('min_range' => $this->min, 'max_range' => $this->max)
+                )
+        );
+    }
+
+    public function getErrorMessageTemplate()
+    {
+        return "The number '{{input}}' must be between {$this->min} and {$this->max}";
+    }
+}
