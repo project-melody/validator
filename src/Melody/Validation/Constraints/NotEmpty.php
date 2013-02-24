9e503a5f6848c329a7e0128d10966321a0d68f73
Marcelo Santos <marcelsud@gmail.com>
2013-02-24 02:24:44 -0300

    Added NotEmpty constraint BETA

---
+++ src/Melody/Validation/Constraints/NotEmpty.php
@@ -0,0 +1,23 @@
+<?php
+namespace Melody\Validation\Constraints;
+
+use Melody\Validation\Validatable;
+
+class NotEmpty extends Constraint implements Validatable
+{
+    protected $id = 'notEmpty';
+
+    public function validate($input)
+    {
+        if (is_string($input)) {
+            $input = trim($input);
+        }
+
+        return !empty($input);
+    }
+
+    public function getErrorMessageTemplate()
+    {
+        return "The input '{{input}}' must not be empty";
+    }
+}
---
+++ tests/src/Melody/Validation/Constraints/NotEmptyTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Melody\Validation\Constraints;
+
+use Melody\Validation\Validator as v;
+
+class NoWhitespaceTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test_valid_string_should_pass()
+    {
+        $this->assertTrue(v::notEmpty()->validate("a"));
+    }
+
+    public function test_invalid_string_should_fail_validation()
+    {
+        $this->assertFalse(v::notEmpty()->validate("     "));
+        $this->assertFalse(v::notEmpty()->validate(" "));
+        $this->assertFalse(v::notEmpty()->validate(""));
+    }
+
+}
